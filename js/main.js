d8c00cf8e64856b44215df51cfb8d2a4933011e6
Bruno <brunofrs7@gmail.com>
2021-03-24 21:41:38 +0000

    JavaScript da Biografia terminado

---
+++ js/main.js
@@ -0,0 +1,22 @@
+ function mostrarCard1(){
+    escondeTextos();
+    document.getElementById('card1').style.display='inline';
+ }
+ function mostrarCard2(){
+    escondeTextos();
+    document.getElementById('card2').style.display='inline';
+}
+function mostrarCard3(){
+    escondeTextos();
+    document.getElementById('card3').style.display='inline';
+}
+function mostrarCard4(){
+    escondeTextos();
+    document.getElementById('card4').style.display='inline';
+}
+function escondeTextos(){
+    document.getElementById('card1').style.display='none';
+    document.getElementById('card2').style.display='none';
+    document.getElementById('card3').style.display='none';
+    document.getElementById('card4').style.display='none';
+}
