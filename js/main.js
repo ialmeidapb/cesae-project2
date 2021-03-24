c6d2e49e62748474041264c34bf95b7099297504
Bruno <brunofrs7@gmail.com>
2021-03-24 22:17:25 +0000

    JavaScript e página Blog terminada

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
- function mostrarCard1(){
+/* --------------------------CARDS - BIOGRAFIA--------------------------------- */
+function mostrarCard1(){
     escondeTextos();
     document.getElementById('card1').style.display='inline';
  }
@@ -19,4 +20,23 @@ function escondeTextos(){
     document.getElementById('card2').style.display='none';
     document.getElementById('card3').style.display='none';
     document.getElementById('card4').style.display='none';
+}
+
+/* --------------------------MODAL/CARDS - BLOG--------------------------------- */
+
+function novoPost(){
+    var titulo = document.getElementById('modal_titulo').value
+    var texto = document.getElementById('modal_texto').value
+    var element = document.getElementById("posts")
+    var numPosts = element.getElementsByClassName('card').length + 1
+    element.innerHTML+='<div id="post'+numPosts+'" class="card mb-1"><div class="card-body"><h5 class="card-title">'+titulo+'</h5><p class="card-text">'+texto+'</p><button class="btn btn-danger" onclick="eliminarPost(\'post'+numPosts+'\')">Eliminar</button> <button class="btn btn-success" onclick="gostar(\'post'+numPosts+'_likes\')">Gostar</button><span class="ml-5" id="post'+numPosts+'_likes">0</span> <i class="fa fa-thumbs-up" aria-hidden="true"></i></div></div>';
+}
+
+function eliminarPost(post){
+    document.getElementById(post).remove()
+}
+
+function gostar(post){
+    var valor = document.getElementById(post).innerText
+    document.getElementById(post).innerText = Number(valor)+1
 }
